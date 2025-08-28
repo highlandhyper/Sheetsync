4e790e19d4ef1e3791e6d51a8cb6fa88b33d498d
Highland Hypermarket <highlandhiper@gmail.com>
2025-08-28 14:44:32 +0000

    i would like to make this in https. i have certificarte in .certs folder

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+
 // @ts-check
-const { createServer } = require('https');
+const { createServer: createHttpServer } = require('http');
+const { createServer: createHttpsServer } = require('https');
 const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
@@ -13,16 +15,29 @@ const handle = app.getRequestHandler();
 const httpPort = 9002;
 const httpsPort = 9003;
 
-// Define SSL options
 const certsDir = path.join(__dirname, '.certs');
+
+// Check if certs exist before trying to read them
+if (!fs.existsSync(path.join(certsDir, 'localhost-key.pem')) || !fs.existsSync(path.join(certsDir, 'localhost.pem'))) {
+  console.error('\n\n\x1b[31m%s\x1b[0m', '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  console.error('\x1b[31m%s\x1b[0m', 'ERROR: SSL CERTIFICATES NOT FOUND');
+  console.error('\x1b[31m%s\x1b[0m', `The server requires SSL certificates in the '.certs' folder but they were not found.`);
+  console.error('\x1b[31m%s\x1b[0m', 'Please ensure `localhost-key.pem` and `localhost.pem` exist in that directory.');
+  console.error('\x1b[31m%s\x1b[0m', 'You can generate them using a tool like `mkcert`.');
+  console.error('\x1b[31m%s\x1b[0m', 'Application will exit.');
+  console.error('\x1b[31m%s\x1b[0m', '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n');
+  process.exit(1);
+}
+
 const httpsOptions = {
   key: fs.readFileSync(path.join(certsDir, 'localhost-key.pem')),
   cert: fs.readFileSync(path.join(certsDir, 'localhost.pem')),
 };
 
+
 app.prepare().then(() => {
   // Create an HTTPS server
-  createServer(httpsOptions, (req, res) => {
+  createHttpsServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   }).listen(httpsPort, (err) => {
@@ -30,13 +45,15 @@ app.prepare().then(() => {
     console.log(`> Ready on https://localhost:${httpsPort}`);
   });
   
-  // Also create an HTTP server for convenience, though HTTPS is primary
-  // This can be removed if only HTTPS is desired.
-   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+  // Create an HTTP server that redirects to HTTPS
+   createHttpServer((req, res) => {
+    const host = req.headers.host || `localhost:${httpPort}`;
+    const httpsUrl = `https://${host.replace(`:${httpPort}`, `:${httpsPort}`)
+}${req.url}`;
+    res.writeHead(301, { Location: httpsUrl });
+    res.end();
   }).listen(httpPort, (err) => {
     if (err) throw err;
-    console.log(`> Also ready on http://localhost:${httpPort}`);
+    console.log(`> HTTP server on http://localhost:${httpPort} is redirecting to HTTPS`);
   });
 });
