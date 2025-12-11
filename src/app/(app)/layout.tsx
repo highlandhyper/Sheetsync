b80d5d54d6b8110f19c51d593cb13eeb4185620f
Highland Hypermarket <highlandhiper@gmail.com>
2025-12-11 14:14:47 +0000

    when opeing sidebar its not adjusting its moving out the screen

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -103,7 +103,7 @@ export default function AppLayout({ children }: PropsWithChildren) {
   return (
     <SidebarProvider defaultOpen={true}>
       <AppSidebar className="noprint" />
-      <SidebarInset className="flex flex-col">
+      <SidebarInset className="flex min-w-0 flex-col">
         <Header className="noprint" />
         <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
           {children}
