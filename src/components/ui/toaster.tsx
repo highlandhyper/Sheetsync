86d92971aa6569ba5106bed234f057ebdf3ae77e
Highland Hypermarket <highlandhiper@gmail.com>
2026-02-03 08:47:33 +0000

    in toast only show main caption dont show sencond line , discription

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,11 +1,9 @@
-
 "use client"
 
 import { useToast } from "@/hooks/use-toast"
 import {
   Toast,
   ToastClose,
-  ToastDescription,
   ToastProvider,
   ToastTitle,
   ToastViewport,
@@ -21,9 +19,6 @@ export function Toaster() {
           <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
             </div>
             {action}
             <ToastClose />
