2f78699274c41ef411e9a0ff29dae876d6a95770
Highland Hypermarket <highlandhiper@gmail.com>
2026-04-06 06:55:10 +0000

    make toast better

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -5,8 +5,8 @@ import {
   Toast,
   ToastClose,
   ToastProvider,
-  ToastTitle,
   ToastViewport,
+  ToastTitle,
   ToastDescription,
 } from "@/components/ui/toast"
 
@@ -16,20 +16,22 @@ export function Toaster() {
   return (
     <ToastProvider swipeDirection="right">
       <ToastViewport />
-      {toasts.map(function ({ id, title, description, action, onOpenChange, ...props }) {
-        return (
-          <Toast key={id} onOpenChange={onOpenChange} {...props} duration={4000}>
-            <div className="flex flex-col justify-center min-w-0 pr-2">
-              {title && <ToastTitle variant={props.variant}>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
+      <div className="flex flex-col gap-3">
+        {toasts.map(function ({ id, title, description, action, onOpenChange, ...props }) {
+          return (
+            <Toast key={id} onOpenChange={onOpenChange} {...props} duration={4000}>
+              <div className="flex flex-col justify-center min-w-0 pr-2">
+                {title && <ToastTitle variant={props.variant}>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+              {action}
+              <ToastClose />
+            </Toast>
+          )
+        })}
+      </div>
     </ToastProvider>
   )
 }
