9c82cf412a842f2c0dab3fcfe0f2de8e4beb32b5
Highland Hypermarket <highlandhiper@gmail.com>
2026-09-17 14:47:15 +0000

    no datas showing in app from db . fix that issue . only fix the issue i

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,4 +1,3 @@
-
 'use server';
 
 import { revalidatePath } from 'next/cache';
@@ -59,7 +58,7 @@ function sanitizeForJSON(input: any): any {
             const value = source[key];
             if (value === null || value === undefined) target[key] = value;
             else if (value instanceof Date) target[key] = value.toISOString();
-            else if (typeof value === 'number') target[key] = (Number.isNaN(input) || !Number.isFinite(input)) ? 0 : value;
+            else if (typeof value === 'number') target[key] = (Number.isNaN(value) || !Number.isFinite(value)) ? 0 : value;
             else if (typeof value === 'object') {
                 const newTarget = Array.isArray(value) ? [] : {};
                 target[key] = newTarget;
